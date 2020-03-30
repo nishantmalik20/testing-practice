92d2c0aa16315161e8e2da8889bd5b86310772a9
Nishant Malik <60120827+nishantmalik20@users.noreply.github.com>
2020-03-30 19:23:38 -0400

    Test Case 4 - Resolved

--- 01-unit/01-sum.js
+++ 01-unit/01-sum.js
@@ -2,6 +2,7 @@
 // DEFINE FUNCTION(S)
 // ==================================================
 function sum(num1, num2) {
+  if (num1 === undefined) num1 = 0;
   if (num2 === undefined) num2 = 0;
   return num1 + num2;
 }
@@ -35,6 +36,8 @@ try {
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = sum();
+  if (result !== 0) throw new Error('Expected sum() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
